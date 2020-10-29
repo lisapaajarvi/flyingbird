567a8da1c19f9c8b98ab0911cffd017084059bd2
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2020-10-29 10:19:29 +0100

    added JSdoc comments

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-window.onload = start;
+window.onload = start; //no need to comment (already commented)
+
+/**
+ * The position of the bird relative to the left side of the page
+ */
 let left = 0;
 
+/** Starts the program on page load */
 function start() {
     fly();
 
-
-function fly() {
-    setInterval(moveBirdToTheRight, 5)
+/** Sets an interval to make the bird fly */
+    function fly() {
+        setInterval(moveBirdToTheRight, 5)
 }
 
 
 }
-
+/** Moves the bird to the right */
 function moveBirdToTheRight() {
     const bird = document.querySelector("img");
 
     //update position
     left += 0.2;
+
     //render position
     bird.style.left = left + "%";
 }
