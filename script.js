04f096b35fcd6cf94a2b3f595abb806509e39ba5
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2020-11-17 09:33:47 +0100

    updated function

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-window.onload = start; //no need to comment (already commented)
+window.onload = start; //no need to comment this
 
 /**
  * The position of the bird relative to the left side of the page
  */
 let left = 0;
 
+
 /** Starts the program on page load */
 function start() {
     fly();
-
+}
 /** Sets an interval to make the bird fly */
-    function fly() {
-        setInterval(moveBirdToTheRight, 5)
+function fly() {
+    setInterval(moveBirdToTheRight, 5);
 }
 
-
-}
-/** Moves the bird to the right */
+/** Moves the bird a small step in percentage to the right */
 function moveBirdToTheRight() {
     const bird = document.querySelector("img");
+
+    if (left >= 100) {
+        left = 0;
+    }
 
     //update position
     left += 0.2;
@@ -26,8 +29,3 @@ function moveBirdToTheRight() {
     //render position
     bird.style.left = left + "%";
 }
-
-
-
-
-//for (let i = 0; i < iterations; i++){}
